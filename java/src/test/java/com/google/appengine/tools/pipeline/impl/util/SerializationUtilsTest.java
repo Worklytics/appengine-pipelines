dd8be55021bd767adb73b06460e5e8648acd7eba
Erik Schultink <eschultink@users.noreply.github.com>
2024-05-16 15:34:36 -0700

    add serialization utils test cases

--- java/src/test/java/com/google/appengine/tools/pipeline/impl/util/SerializationUtilsTest.java
+++ java/src/test/java/com/google/appengine/tools/pipeline/impl/util/SerializationUtilsTest.java
@@ -1,6 +1,8 @@
 package com.google.appengine.tools.pipeline.impl.util;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
 
@@ -9,11 +11,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class SerializationUtilsTest {
 
 
-  @Test
-  public void roundtrip() throws IOException {
+  // range of values, so test with both compressed and not compressed
+  //TODO: 1e6, 2e6 cases slow ... 7s, 13s each - wtf
+  @ParameterizedTest
+  @ValueSource(ints = {5, 100, 1000, 10_000, 1_000_000, 2_000_000})
+  public void roundtrip(int longs) throws IOException {
 
-    //2M longs, not random, should be OK
-    long[] largeValue = new long[2000000];
+    long[] largeValue = new long[longs];
     for (int i = 0; i < largeValue.length; i++) {
       largeValue[i] = i;
     }
