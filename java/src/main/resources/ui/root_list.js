4cc2d08fac245b953f65237437744a3ae31560ae
davidfq <davidfq@gmail.com>
2024-05-20 18:20:25 +0200

    Fix XSRF 2nd attempt

--- java/src/main/resources/ui/root_list.js
+++ java/src/main/resources/ui/root_list.js
@@ -21,9 +21,10 @@
 
 function initRootList() {
   setButter('Loading root jobs...');
+  var query = new URLSearchParams(location.search);
   $.ajax({
     type: 'GET',
-    url: 'rpc/list' + _.escape(window.location.search),
+    url: 'rpc/list' + '?' + _.escape(query.toString()),
     dataType: 'text',
     error: function(request, textStatus) {
       getResponseDataJson(textStatus);
