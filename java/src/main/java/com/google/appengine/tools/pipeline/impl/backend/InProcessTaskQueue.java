fdcc0a40e0d8b85e384b06e90c04063d88b92cea
Erik Schultink <eschultink@users.noreply.github.com>
2025-03-21 14:06:42 -0700

    fix complie

--- java/src/main/java/com/google/appengine/tools/pipeline/impl/backend/InProcessTaskQueue.java
+++ java/src/main/java/com/google/appengine/tools/pipeline/impl/backend/InProcessTaskQueue.java
@@ -2,6 +2,7 @@ package com.google.appengine.tools.pipeline.impl.backend;
 
 import com.google.appengine.tools.pipeline.impl.servlets.TaskHandler;
 import com.google.appengine.tools.pipeline.impl.tasks.PipelineTask;
+import com.google.cloud.datastore.Transaction;
 
 import java.time.Instant;
 import java.util.*;
@@ -32,6 +33,11 @@ public class InProcessTaskQueue implements PipelineTaskQueue {
   @Override
   public Collection<TaskReference> enqueue(Collection<PipelineTask> pipelineTasks) {
     return pipelineTasks.stream().map(this::enqueue).collect(Collectors.toCollection(LinkedList::new));
+  }
+
+  @Override
+  public Collection<TaskReference> enqueue(Transaction txn, Collection<PipelineTask> pipelineTasks) {
+    return enqueue(pipelineTasks);
   }
 
   @Override
