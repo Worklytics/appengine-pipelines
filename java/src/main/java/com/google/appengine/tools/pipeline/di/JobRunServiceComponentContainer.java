1672fdbe23920d8ef8b6053c2e99d561ae7cd629
Erik Schultink <eschultink@users.noreply.github.com>
2025-03-28 11:44:26 -0700

    rest of changes

--- java/src/main/java/com/google/appengine/tools/pipeline/di/JobRunServiceComponentContainer.java
+++ java/src/main/java/com/google/appengine/tools/pipeline/di/JobRunServiceComponentContainer.java
@@ -1,4 +1,17 @@
 package com.google.appengine.tools.pipeline.di;
 
-public class JobRunServiceComponentContainer {
+
+
+public final class JobRunServiceComponentContainer {
+  private JobRunServiceComponentContainer() {}
+
+  // suggested by chat-gpt to get static thread-safe initialization with min synchronization overhead
+  private static class Holder {
+    private static final JobRunServiceComponent INSTANCE =
+      DaggerJobRunServiceComponent.create();
+  }
+
+  public static JobRunServiceComponent getInstance() {
+    return Holder.INSTANCE;
+  }
 }
