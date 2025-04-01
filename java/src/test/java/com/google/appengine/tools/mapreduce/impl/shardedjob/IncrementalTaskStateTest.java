76c6abdc075492f8a6ef38ac14c26a77b39cda39
Jose Lorenzo <jose@worklytics.co>
2025-04-01 10:35:00 -0700

    Fix test

--- java/src/test/java/com/google/appengine/tools/mapreduce/impl/shardedjob/IncrementalTaskStateTest.java
+++ java/src/test/java/com/google/appengine/tools/mapreduce/impl/shardedjob/IncrementalTaskStateTest.java
@@ -1,12 +1,15 @@
 package com.google.appengine.tools.mapreduce.impl.shardedjob;
 
+import com.google.appengine.tools.pipeline.DatastoreExtension;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Key;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+@ExtendWith(DatastoreExtension.class)
 class IncrementalTaskStateTest {
 
   @Test
