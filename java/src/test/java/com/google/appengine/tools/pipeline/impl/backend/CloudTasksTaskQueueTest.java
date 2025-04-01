44549d7c621428dbb7bc6dfe11c63adea73132f4
Erik Schultink <eschultink@users.noreply.github.com>
2025-04-01 08:06:23 -0700

    add a mapping test for TaskSpec --> Cloud Task

--- java/src/test/java/com/google/appengine/tools/pipeline/impl/backend/CloudTasksTaskQueueTest.java
+++ java/src/test/java/com/google/appengine/tools/pipeline/impl/backend/CloudTasksTaskQueueTest.java
@@ -3,12 +3,17 @@ package com.google.appengine.tools.pipeline.impl.backend;
 import com.google.appengine.tools.pipeline.testutil.FakeAppEngineEnvironment;
 import com.google.appengine.tools.pipeline.testutil.FakeAppEngineServicesService;
 import com.google.cloud.tasks.v2.CloudTasksClient;
+import com.google.cloud.tasks.v2.HttpMethod;
+import com.google.cloud.tasks.v2.QueueName;
+import com.google.cloud.tasks.v2.Task;
+import com.google.common.collect.ImmutableMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Provider;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CloudTasksTaskQueueTest {
 
@@ -51,6 +56,23 @@ public class CloudTasksTaskQueueTest {
     assertEquals("us-west2", cloudTasksTaskQueue.getQueueLocation());
 
     System.clearProperty(CloudTasksTaskQueue.ConfigProperty.CLOUDTASKS_QUEUE_LOCATION.name());
+  }
+
+
+  @Test
+  public void taskSpec() {
+    Task task = cloudTasksTaskQueue.toCloudTask(QueueName.of("test-project", "us-central", "test-queue"),
+      CloudTasksTaskQueue.TaskSpec.builder()
+        .host("test-host")
+        .method(CloudTasksTaskQueue.TaskSpec.Method.GET)
+        .callbackPath("/test/path")
+        .headers(ImmutableMap.of("key", "value"))
+        .build());
+
+    assertEquals("test-host", task.getAppEngineHttpRequest().getHeaders().get("Host"));
+    assertEquals(HttpMethod.GET, task.getAppEngineHttpRequest().getHttpMethod());
+    assertEquals("/test/path?", task.getAppEngineHttpRequest().getRelativeUri());
+    assertTrue( task.getAppEngineHttpRequest().getHeadersMap().containsKey("key"));
   }
 
 
